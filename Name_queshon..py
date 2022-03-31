e0f9d003abc744620afd1d968db08e56f30b95d8
devikasin <devika21@navgurukul.org>
2022-03-31 23:41:35 +0530

    added one more file

--- Name_queshon..py
+++ Name_queshon..py
@@ -1,20 +1,20 @@
-# a="my name is devika"
-# # ["my","name","is","devika"]
-# i=0
-# while i<len(a):
-#     l=a.split()
-#     i=i+1
-# print(l)   
-
-
-a=["my", "name", "is", "devika"]
-# o/p mynameis devika
-string=" "
+a="my name is devika"
+# ["my","name","is","devika"]
 i=0
-while i<len (a):
-    string=string+str(a[i])
+while i<len(a):
+    l=a.split()
     i=i+1
-print(string)
+print(l)   
+
+
+# a=["my", "name", "is", "devika"]
+# # o/p mynameis devika
+# string=" "
+# i=0
+# while i<len (a):
+#     string=string+str(a[i])
+#     i=i+1
+# print(string)
 
 
 # a=["my","name","is","devika"]
---
+++ [1,1,1,1,1]_wala_q.py
@@ -0,0 +1,10 @@
+List=[1,2,3,4,5]
+a=1
+b=[]
+i=0
+while i<len(List):
+    c=List[i-a+1]
+    b.append(c)
+    a=a+1
+    i=i+1
+print(b)
